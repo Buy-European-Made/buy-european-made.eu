1b6e27f93d53586b50560e5b65b75e2176e6aac6
Louis VINCHON <louis.vinchon.dev@gmail.com>
2025-03-22 16:50:51 +0100

    fix: Set a fixed display size for products' icons

--- src/blocks/ProductList/product/Component.tsx
+++ src/blocks/ProductList/product/Component.tsx
@@ -15,7 +15,7 @@ export const ProductCard: React.FC<ProductCardProps> = async ({ product }) => {
       <div className="max-w-[48rem]">
         { product.logo &&
           <Media
-            imgClassName='border border-border rounded-[0.8rem]'
+            imgClassName='h-32 w-32 border border-border rounded-[0.8rem]'
             resource={product.logo}
           />
         }
