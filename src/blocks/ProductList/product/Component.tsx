21f84d9dd04adcf9adea4733609572f237f624a8
fedmag <maggiorefederico@outlook.com>
2025-04-13 19:21:03 +0200

    adds conditional rendering

--- src/blocks/ProductList/product/Component.tsx
+++ src/blocks/ProductList/product/Component.tsx
@@ -12,11 +12,12 @@ interface ProductCardProps {
 }
 
 export const ProductCard: React.FC<ProductCardProps> = async ({ product }) => {
-
+  const hasCategory = product.categories !== null && product.categories !== undefined && product.categories.length !== 0
+  const hasSubcategory = product.subcategories !== null && product.subcategories !== undefined && product.subcategories.length !== 0
+  const replacesProducts = product.replaces !== null && product.replaces !== undefined && product.replaces.length !== 0
   return (
     <>
       <Card className='h-full flex flex-col'>
-
         <CardHeader>
           <div className='flex flex-col md:flex-row justify-between'>
             <div className='flex items-center justify-center'>
@@ -46,55 +47,67 @@ export const ProductCard: React.FC<ProductCardProps> = async ({ product }) => {
           <CardDescription className='text-ellipsis overflow-hidden h-fit p-6' >
             {product.description}
           </CardDescription>
-
           <div className='flex flex-col'>
             <CardContent className='flex flex-col justify-end text-xs gap-2'>
+              {hasCategory && (
+                <div>
+                  <div className='flex items-center'>
+                    <p className='font-semibold text-gray-800 dark:text-gray-300'>Category:</p>
+                  </div>
+                  <div className='flex gap-2 flex-wrap'>
+                    {product.categories?.map((category: Category | number) => {
+                      if (category !== undefined && typeof category !== 'number') {
+                        return (
+                          <p key={category.id} className='text-blue-500 font-medium border-2 border-blue-500 p-1 rounded'>
+                            {category.name}
+                          </p>
+                        );
+                      }
+                      return null;
+                    })}
+                  </div>
+                </div>
+              )}
 
-              <div className='flex items-center '>
-                <p className='font-semibold text-gray-800 dark:text-gray-300'> Category: </p>
-              </div>
-              <div className='flex gap-2 flex-wrap' >
-                {product.categories?.map((category: Category | number) => {
-                  if (category !== undefined && typeof category !== 'number') {
-                    return (
-                      <p key={category.id} className='text-blue-500 font-medium border-2 border-blue-500 p-1 rounded '>
-                        {category.name}
-                      </p>
-                    )
-                  }
-                })}
-              </div>
-              <div className='flex items-center '>
-                <p className='font-semibold text-gray-800 dark:text-gray-300'> Subcategory: </p>
-              </div>
-              <div className='flex gap-2 flex-wrap' >
-                {product.subcategories?.map((subcategory: Subcategory | number) => {
-                  if (subcategory !== undefined && typeof subcategory !== 'number') {
-                    return (
-                      <p key={subcategory.id} className='text-yellow-500 font-medium border-2 border-yellow-500 p-1 rounded '>
-                        {subcategory.name}
-                      </p>
-                    )
-                  }
-                })}
-              </div>
+              {hasSubcategory && (
+                <div>
+                  <div className='flex items-center'>
+                    <p className='font-semibold text-gray-800 dark:text-gray-300'>Subcategory:</p>
+                  </div>
+                  <div className='flex gap-2 flex-wrap'>
+                    {product.subcategories?.map((subcategory: Subcategory | number) => {
+                      if (subcategory !== undefined && typeof subcategory !== 'number') {
+                        return (
+                          <p key={subcategory.id} className='text-yellow-500 font-medium border-2 border-yellow-500 p-1 rounded'>
+                            {subcategory.name}
+                          </p>
+                        );
+                      }
+                      return null;
+                    })}
+                  </div>
+                </div>
+              )}
 
-              <div className='flex items-center '>
-                <p className='font-semibold text-gray-800 dark:text-gray-300'> Replaces: </p>
-              </div>
-              <div className='flex gap-2 flex-wrap' >
-                {product.replaces?.map((replacedProduct: ReplacedProduct | number) => {
-                  if (replacedProduct !== undefined && typeof replacedProduct !== 'number') {
-                    return (
-                      <p key={replacedProduct.id} className='text-gray-500 font-medium  border-2 border-gray-500 p-1 rounded'>
-                        {replacedProduct.name}
-                      </p>
-                    )
-                  }
-                })}
-
-              </div>
-
+              {replacesProducts && (
+                <div>
+                  <div className='flex items-center'>
+                    <p className='font-semibold text-gray-800 dark:text-gray-300'>Replaces:</p>
+                  </div>
+                  <div className='flex gap-2 flex-wrap'>
+                    {product.replaces?.map((replacedProduct: ReplacedProduct | number) => {
+                      if (replacedProduct !== undefined && typeof replacedProduct !== 'number') {
+                        return (
+                          <p key={replacedProduct.id} className='text-gray-500 font-medium border-2 border-gray-500 p-1 rounded'>
+                            {replacedProduct.name}
+                          </p>
+                        );
+                      }
+                      return null;
+                    })}
+                  </div>
+                </div>
+              )}
             </CardContent>
             <CardFooter className='flex gap-4 mt-3'>
               <Button>
