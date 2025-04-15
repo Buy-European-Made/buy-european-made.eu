d8dd2e6396be51b5c882cbba40620736daf61d97
fedmag <maggiorefederico@outlook.com>
2025-04-15 11:17:48 +0200

    fix responsive design?

--- src/blocks/ProductList/Component.tsx
+++ src/blocks/ProductList/Component.tsx
@@ -10,7 +10,7 @@ export const ProductsListBlock: React.FC = async () => {
 
   if (findResult.docs.length > 0) {
     return (
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mx-3'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-3'>
         {
           findResult.docs.map((product) => {
             return <ProductCard
